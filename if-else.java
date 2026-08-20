5ada894ced80fd37af31f7d69caf60dfe980ef3f
Arun <arunrajput1836@gmail.com>
2026-08-20 22:27:39 +0530

    Add nested if-else logic to ifelse class
    Implemented a nested if-else structure to evaluate conditions based on the variable 'a'. The structure provides additional output based on the value of 'a' being greater or less than 10.

--- if-else.java
+++ if-else.java
@@ -9,3 +9,22 @@ public class ifelse{
             System.out.println("odd");
         }
     }
+
+// nested if
+public class ifelse{
+    public static void main (String args []){
+        int a = 19;
+        if (a > 10){
+            System.out.println("right");
+
+                if (a < 10){
+
+                    System.out.println("wrong");
+                }else{
+                     System.out.println("nice");
+                }
+            }else{              
+                    System.out.println("good");
+                }
+        }
+    }
