8d55ada083914cd4dc5c58a376ad42acfaa916ec
Arun <arunrajput1836@gmail.com>
2026-08-24 14:05:38 +0530

    Add logic to find greater of two numbers

--- if-else.java
+++ if-else.java
@@ -28,3 +28,16 @@ public class ifelse{
                 }
         }
     }
+
+//grater of two no
+public class ifelse{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+    if (a > b){
+        System.out.println("a is freater ");
+    }else{
+    System.out.println("b is greater");
+        }
+    }
+}
