2c0203ca1085c585481796d6b267cba27d083911
Arun <arunrajput1836@gmail.com>
2026-08-26 10:55:12 +0530

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -20,3 +20,36 @@ public class array{
 
     }
 }
+
+
+import java.util.Scanner;
+
+public class demoFirst{
+    public static void main(String []args){
+        int n =10;
+        float ar[]=new float[n];
+        Scanner sc = new Scanner(System.in);
+
+        for(int i = 0;i<n;i++)
+        {
+            ar[i]= sc.nextFloat();
+
+        }
+        float max=ar[0];
+        float min=ar[0];
+        float sum=0;
+        for(float p:ar)
+        {
+            if(p>max)
+                max=p;
+            if(p<min)
+                min=p;
+            sum=sum+p;
+
+        }
+        System.out.println("total Billl" +sum);
+        System.out.println("total Billl" +max);
+        System.out.println("total Billl" +min);
+        System.out.println("total Billl" +sum/n);
+    }
+}
