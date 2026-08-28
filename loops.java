79ea370fb11c90baead0c66a223af5bd5a606d3b
Arun <arunrajput1836@gmail.com>
2026-08-28 21:51:44 +0530

    Add loop to print numbers from 1 to 15
    Implement a loop to display numbers from 1 to n.

--- loops.java
+++ loops.java
@@ -1,1 +1,10 @@
 // loops in java program and solutions
+// Display number from 1 to n
+public class loops{
+    public static void main(String args[]){
+        int i, n= 15;
+        for(i = 1; i <= n ; i++){
+            System.out.println(i);
+        }
+    }
+}
