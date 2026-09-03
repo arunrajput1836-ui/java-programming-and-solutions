72e3aca01095c011cb95f9300f37a03b8186b369
Arun <arunrajput1836@gmail.com>
2026-09-04 00:21:35 +0530

    Implement digit counting in loops.java
    Added functionality to count the number of digits in an input number.

--- loops.java
+++ loops.java
@@ -98,3 +98,23 @@ public class loop{
     }
 }
 
+// count digit 
+     import java.util.Scanner;
+public class loops{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+       
+        int count = 0;
+
+        while(n>0){
+        n = n/10 ;
+        count++;
+      
+    }
+         System.out.println(count);
+      sc.close();
+}
+}
+
