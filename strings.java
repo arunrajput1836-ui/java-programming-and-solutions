fca98dd57a54f109bf8ac7faed8e0ab113ad8f94
Arun <arunrajput1836@gmail.com>
2026-09-09 10:42:28 +0530

    Update strings.java

--- strings.java
+++ strings.java
@@ -15,3 +15,31 @@ public class string {
     }
     
 }
+
+// name search
+public class namesearch{
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 10;
+        String name[] = new String[n];
+
+        for(int i = 0;i<n;i++)
+            name[i]=sc.nextLine();
+
+        System.out.println("Enter searching name");
+        String SearchName = sc.nextLine();
+        int flag =0;
+        for(int i =0;i<n;i++)
+        {
+            if (name[i].equalsIgnoreCase(SearchName))
+            {
+                flag =1;
+                break;
+            }
+        }
+        if(flag ==0)
+            System.out.println("name not found");
+        else
+            System.out.println("name found");
+     }
+}
