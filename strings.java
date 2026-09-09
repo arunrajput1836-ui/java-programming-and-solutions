83d94f0eef84d81784f774da6c567777b12b98f7
Arun <arunrajput1836@gmail.com>
2026-09-09 10:27:49 +0530

    Update strings.java

--- strings.java
+++ strings.java
@@ -1,1 +1,17 @@
 
+// Bank Account no masking.
+import java.util.Scanner;
+public class string {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Acoount no");
+        String acc = sc.nextLine();
+        char ar[] = acc.toCharArray();
+        for(int i = 0;i<ar.length-4;i++)
+            ar[i]  ='*';
+
+        String maskedAcc = new String(ar);
+        System.out.println(maskedAcc);
+    }
+    
+}
