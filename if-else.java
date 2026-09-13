0df117d68fd23dac674a8da4ec3547c4b26508aa
Arun <arunrajput1836@gmail.com>
2026-09-13 22:54:53 +0530

    Update if-else.java

--- if-else.java
+++ if-else.java
@@ -116,3 +116,18 @@ public class ifelse{
                         System.out.println("fail");
                     }
                 }
+
+    // largest no;
+public class ifelse{
+    public static void main(String args[]){
+        int a = 100, b = 15, c = 20;
+        if(a>b && a > c){
+            System.out.println("a is largest");}
+        else if (b > a && b > c){
+            System.out.println("b is largest");}
+        else if (c>a && c> b){
+            System.out.println("c is largest");
+        }
+
+    }
+}
