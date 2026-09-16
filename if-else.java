a8cf5901b7453585992decc0ece8c44aa48d6183
Arun <arunrajput1836@gmail.com>
2026-09-16 22:45:09 +0530

    Implement prime number checking logic
    Added a program to check if a number is prime or not.

--- if-else.java
+++ if-else.java
@@ -266,3 +266,30 @@ public class Array{
       }
    }
 }
+
+
+
+// // // import java.util.Scanner;
+public class loops {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the no");
+        int n=sc.nextInt();
+        int flag=0;
+        for(int i=2;i<=n/2;i++){
+            if(n%i==0)
+            { 
+                flag=1;
+                break;
+            }
+        }
+        if(flag==1){
+            System.out.println("not prime");
+        }else{
+            System.out.println("prime")
+        }
+
+
+    }
+    
+// // }
